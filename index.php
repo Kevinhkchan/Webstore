9bab9bad233e035b3ba7270fea033c9fb1b65bbd
Kevinhkchan <34487956+Kevinhkchan@users.noreply.github.com>
2018-01-02 21:09:09 -0600

    Index Page
    This page will be the home page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css"
+</head>
+
+<body>
+
+<img src="images/Design.jpeg" alt="PathtoTCGs Logo" height="300" width="100%">
+<! Navigation Bar -->
+<ul>
+    <li><a href="index.php"> Home Screen </a> </li>
+    <li><a href="cardList.php"> Cards Available </a> </li>
+    <li><a href="about.php">About Page </a> </li>
+</ul>
+
+<h1> Site Currently In Construction </h1>
+
+</body>
+
+</html>

64437492335b6078f6eb85900554e339b6c88e82
Kevinhkchan <34487956+Kevinhkchan@users.noreply.github.com>
2018-01-02 21:07:58 -0600

    Add files via upload
    About page for website

---
+++ about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css"
+</head>
+
+<body>
+<img src="images/Design.jpeg" alt="PathtoTCGs Logo" height="300" width="100%">
+<! Navigation Bar -->
+    <ul>
+        <li><a href="index.php"> Home Screen </a> </li>
+        <li><a href="cardList.php"> Cards Available </a> </li>
+        <li><a href="about.php">About Page </a> </li>
+    </ul>
+
+<h1> About </h1>
+Path to TCGs strives to bring quality cards along with swift delivery! <br>
+
+<h2> Youtube Channel </h2>
+We also have a YouTube channel that we are striving to bring more content for . <br>
+
+<b> Youtube Channel: </b> <a href="http://www.youtube.com/PathtoTCGs"> http://www.youtube.com/PathtoTCGs </a>
+</body>
+
+</html>
